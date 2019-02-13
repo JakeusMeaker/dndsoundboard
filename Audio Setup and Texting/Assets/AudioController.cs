66419995fcd658926b0b586ccf1ba436fc703965
Jake Meaker <JM219785@falmouth.ac.uk>
2019-02-13 12:27:51 +0000

    Initial Commit

---
+++ Audio Setup and Texting/Assets/AudioController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+    AudioSource track;
+
+    bool playState = false;
+
+	// Use this for initialization
+	void Start () {
+        track = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayStop()
+    {
+        if (!playState)
+        {
+            track.Play();
+        }
+        else
+        {
+            track.Stop();
+        }
+
+        playState = !playState;
+    }
+}
